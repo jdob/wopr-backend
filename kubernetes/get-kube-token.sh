f82c374f77ed8c8d5a4c725b642c5dd2d67e9a22
Jay Dobies <jason.dobies@redhat.com>
2019-11-18 12:12:00 -0800

    Updates to kube handling

--- kubernetes/get-kube-token.sh
+++ kubernetes/get-kube-token.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SA_NAME=wopr
-export TOKEN=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='$SA_NAME')].data.token}"|base64 -d)
+export TOKEN=$(kubectl get secrets -n wopr -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='$SA_NAME')].data.token}"|base64 --decode)
 
 echo ""
 echo $TOKEN
